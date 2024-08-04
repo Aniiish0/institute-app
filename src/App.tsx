89ba9ea1936d4151bde7eab88f7f79a687893f2b
Subrat Kumar Pattanaik <subratpatnaik.email@gmail.com>
2024-08-04 15:51:42 +0530

    feat: Homepage and header (#12)
    * changes for homePage and navbar
    
    * enhanced homePage
    
    * feat: homepage and navbar development
    
    ---------
    
    Co-authored-by: swagatika1999 <42492831+swagatika1999@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ContactForm from './components/widgets/ContactForm'
 import FAQ from './components/widgets/FAQs'
+import HomePage from './components/HomePage'
+import HomeNavbar from './components/Navbar'
 
 function App() {
   return (
     <div className='container-fluid'>
+      <HomeNavbar />
+      <HomePage />
+
       <FAQ />
       <ContactForm />
     </div>
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,69 @@
+import { Button } from 'react-bootstrap';
+
+const HomeNavbar = () => {
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Logo
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className="mx-auto" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Contact us
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Events
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Class
+            </a>
+            <div
+              className="dropdown-menu"
+              aria-labelledby="navbarDropdownMenuLink"
+            >
+              <a className="dropdown-item" href="#">
+                LKG
+              </a>
+              <a className="dropdown-item" href="#">
+                UKG
+              </a>
+              <a className="dropdown-item" href="#">
+                class 1
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <Button variant="primary" >
+          <i className="bi bi-download"/>
+          <span style={{paddingLeft:"10px"}}>Brochure</span>
+      </Button>
+      </div>
+    </nav>
+    
+  );
+};
+export default HomeNavbar;
