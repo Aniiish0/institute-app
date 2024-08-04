4578472304fd717c331574d1727b5475488a9ed3
Anish Panda <42354469+Anish005@users.noreply.github.com>
2024-08-04 15:53:22 +0530

    feat: brand color changes (#22)
    - the brand color was set to #0644a7
    
    Co-authored-by: swagatika1999 <42492831+swagatika1999@users.noreply.github.com>

--- src/components/widgets/FAQs.tsx
+++ src/components/widgets/FAQs.tsx
@@ -17,7 +17,7 @@ const faq: React.FC = () => {
 
   return (
     <Container>
-      <h1>FAQs</h1>
+      <h1 className='custom-h1'>FAQs</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
       <div className='my-4'>
         <Row xs={1} sm={1} md={1} lg={2} className="g-2">
