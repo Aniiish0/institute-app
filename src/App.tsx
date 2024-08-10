cffe9d703c01add624eb80782d1a8802c36316dc
swagatika1999 <42492831+swagatika1999@users.noreply.github.com>
2024-08-10 10:25:56 +0000

    feat: homePage and navbar enhancement and separate component for learning activities
    - homepage and navbar enhancement
    - component for learning activities added
    - replaced lorem epsum with example content
    - fixed navbar collapse button issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,27 @@ import FAQ from "./components/widgets/FAQs";
 import HomePage from "./components/HomePage";
 import HomeNavbar from "./components/Navbar";
 import Highlight1 from "./components/widgets/Highlight1";
+import Highlight2 from "./components/widgets/Highlight2";
 
 function App() {
   return (
-    <div className="container-fluid">
+    <div>
       <HomeNavbar />
-      <br />
-      <HomePage />
-      <br />
-      <Highlight1 />
-      <br />
-      <FAQ />
-      <br />
-      <ContactForm />
-      <br />
+      <div className="container-fluid">
+        <section id="home">
+          <HomePage />
+        </section>
+        <section id="events">
+          <Highlight1 />
+          <Highlight2 />
+        </section>
+        <section>
+          <FAQ />
+        </section>
+        <section id="contact-us">
+          <ContactForm />
+        </section>
+      </div>
     </div>
   );
 }
