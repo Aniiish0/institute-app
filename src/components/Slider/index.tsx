9543f3951c40962f4c3960b6f7b81eae19df8bb7
swagatika1999 <42492831+swagatika1999@users.noreply.github.com>
2024-08-18 20:47:13 +0530

    refactor: enhanced slider component (#53)
    Co-authored-by: Swagatika Mishra <swagatikamishra@Swagatikas-MacBook-Air.local>

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,54 +1,29 @@
+// ImageCarousel.tsx
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./slider.css";
 
-const Slider = () => {
+interface Slider {
+  images: string[];
+}
+
+const Slider: React.FC<Slider> = ({ images }) => {
   return (
-    <div
-      id="carouselExampleControls"
-      className="carousel slide"
-      data-bs-ride="carousel"
+    <Carousel
+      showThumbs={false}
+      infiniteLoop={true}
+      useKeyboardArrows
+      autoPlay
+      interval={5000}
+      showIndicators={false}
     >
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img
-            src="https://media.istockphoto.com/id/2149774102/photo/smiling-black-schoolboy-writing-on-a-class-at-elementary-school.webp?b=1&s=170667a&w=0&k=20&c=BR9RTWiSEYLAcFVEEkMDBXjYs12eS8FSRMTG0oNdVHE="
-            className="d-block w-100"
-            alt="..."
-          />
+      {images.map((image, index) => (
+        <div className="carousel-image-container" key={index}>
+          <img src={image} alt="" className="carousel-image" />
         </div>
-        <div className="carousel-item">
-          <img
-            src="https://media.istockphoto.com/id/1401178460/photo/teacher-helping-a-young-student-with-her-homework-in-the-library-after-school-two-females-are.webp?b=1&s=170667a&w=0&k=20&c=aPY2PqE2XJtiRo-9WSdDR0DHTy3PgocUK0GVTwoYmcg="
-            className="d-block w-100"
-            alt="..."
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://media.istockphoto.com/id/1409722748/photo/students-raising-hands-while-teacher-asking-them-questions-in-classroom.webp?b=1&s=170667a&w=0&k=20&c=wmjzzESyHcSDFXYI1qzngse-EIj7aBMfebEaqjT8cjM="
-            className="d-block w-100"
-            alt="..."
-          />
-        </div>
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
-    </div>
+      ))}
+    </Carousel>
   );
 };
 
