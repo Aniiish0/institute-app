a3ce661191c550f8daeea5d0e04f3087cb3580e1
Swagatika Mishra <swagatikamishra@Swagatikas-MacBook-Air.local>
2024-08-18 20:24:03 +0530

    refactor: enhanced slider component

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -2,6 +2,11 @@ import Slider from "../Slider";
 import "./HomePage.css";
 import { Container } from "react-bootstrap";
 const HomePage = () => {
+  const images = [
+    "https://plus.unsplash.com/premium_photo-1663106423058-c5242333348c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2Nob29sJTIwa2lkc3xlbnwwfHwwfHx8MA%3D%3D",
+    "https://media.istockphoto.com/id/1401178460/photo/teacher-helping-a-young-student-with-her-homework-in-the-library-after-school-two-females-are.webp?b=1&s=170667a&w=0&k=20&c=aPY2PqE2XJtiRo-9WSdDR0DHTy3PgocUK0GVTwoYmcg=",
+    "https://media.istockphoto.com/id/1409722748/photo/students-raising-hands-while-teacher-asking-them-questions-in-classroom.webp?b=1&s=170667a&w=0&k=20&c=wmjzzESyHcSDFXYI1qzngse-EIj7aBMfebEaqjT8cjM=",
+  ];
   return (
     <Container className="container-fluid">
       <div>
@@ -19,7 +24,7 @@ const HomePage = () => {
         children to learn, study, and develop essential skills.
       </div>
       <br></br>
-      <Slider />
+      <Slider images={images} />
     </Container>
   );
 };
